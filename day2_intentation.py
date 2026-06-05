1412eac967a56a51a272a7cbd3a0ed1d72e113b0
Urvashi <urrvashii99@gmail.com>
2026-06-05 17:44:24 +0530

    Implement basic Python syntax examples
    Added comments and examples for various operators and indentation in Python.

---
+++ day2_intentation.py
@@ -0,0 +1,28 @@
+#indentation /comments/variables/datattypes
+if 2==2:
+    print("both values are same")
+    
+#I am a comment
+'''i 
+   am
+  multi-line
+  string documentation
+  '''
+  
+#some operators
+#membership
+print("i" in name)
+print( "z" not in name)
+#identity
+print(age is height )
+print(age is not height )
+#logical
+print(2|2)
+print(3&5)
+#comparision
+print(3==3)
+print(5!=4)
+print(5<=6)
+print(3>=1)
+print(4>2)
+print(2<9)
