1f36193acb877797d5a38d44cb2a2fdffc540a1d
Urvashi <urrvashii99@gmail.com>
2026-06-05 17:47:28 +0530

    Add variable declarations and type checks in Python

---
+++ day4_datatypes.py
@@ -0,0 +1,18 @@
+#variables name 
+name = "urvashi"
+age = 20
+height = 5.2
+_class = "BCA"
+roll_no = 36472
+is_student = True
+com = 3+4j
+
+#check type of variable
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(_class))
+print(type(roll_no))
+print(type(is_student))
+print(type(com))
